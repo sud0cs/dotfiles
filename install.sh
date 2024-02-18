f1c9cd27ff6a2b4a2f231b6e6916bf9896451864
sud0cs <asintessintes@gmail.com>
2024-02-18 20:41:33 +0100

    compfy and install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -x "$(command -v apt-get)" ];
 then
-  sudo apt-get install python3 python3-pip kitty rofi firefox flameshot wget libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev unzip
+  sudo apt-get install python3 python3-pip kitty rofi firefox flameshot wget libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev unzip -y
   git clone https://github.com/allusive-dev/compfy
   cd compfy
   meson setup . build
@@ -28,14 +28,14 @@ elif [ -x "$(command -v dnf)" ];
 then
   echo "dnf"
 fi
-cp -r dotfiles/awesome ~/.config/ && \
-cp -r dotfiles/kitty ~/.config/ && \
-cp -r dotfiles/rofi ~/.config/ && \
-cp -r dotfiles/autocolor ~/.config/ && \
-cp -r dotfiles/.wallpapers ~
-cp -r dotfiles/firefox/homepage ~ && \
-cp -r dotfiles/firefox/chrome ~/.mozilla/firefox/*.default-release/ && \
-sudo cp -r dotfiles/picom/picom.conf /etc/xdg/
+cp -r awesome ~/.config/ && \
+cp -r kitty ~/.config/ && \
+cp -r rofi ~/.config/ && \
+cp -r autocolor ~/.config/ && \
+cp -r .wallpapers ~
+cp -r firefox/homepage ~ && \
+cp -r firefox/chrome ~/.mozilla/firefox/*.default-release/ && \
+cp -r compfy ~/.config/
 cd ~/.config/autocolor && \
 python3 -m venv venv && \
 source venv/bin/activate && \
